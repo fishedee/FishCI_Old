7825abd4f549729d08a1898ec90532c585c372d6
fishedee <306766045@qq.com>
2015-02-04 17:58:06 +0800

    去掉不必要的变量声明

--- system/libraries/MyException.php
+++ system/libraries/MyException.php
@@ -1,8 +1,5 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class CI_MyException extends Exception{ 
-
-	var $code;
-	var $message;
 	var $data;
 	
     public function __construct($code = 1,$message = '',$data = '') {  
